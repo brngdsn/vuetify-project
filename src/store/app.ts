c12f5cb00d7ac2f36e97a5c937359c2125727a19
Braun Goodson <brn.gdsn@gmail.com>
2024-01-24 09:58:50 -0500

    Update app.ts

--- src/store/app.ts
+++ src/store/app.ts
@@ -3,6 +3,20 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    data: null,
+    loading: false,
+    error: null,
   }),
+  async fetchData() {
+    this.loading = true;
+    this.error = null;
+    try {
+      const response = await axios.get('https://data.nasa.gov/resource/y77d-th95.json');
+      this.data = response.data;
+    } catch (error) {
+      this.error = error;
+    } finally {
+      this.loading = false;
+    }
+  }
 })
