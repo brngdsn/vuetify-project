64c533db1604d28f1d522d0e7e39543b6e5798df
EC2 Default User <ec2-user@ip-172-31-46-80.ec2.internal>
2024-01-24 15:14:23 +0000

    add axios; fix store import;

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -71,7 +71,7 @@
 </template>
 
 <script setup lang="ts">
-  import { useAppStore } from './store';
+  import { useAppStore } from '@/store/app';
   const appStore = useAppStore();
   
   onMounted(async () => {
