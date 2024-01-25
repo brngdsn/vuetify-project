d2b99054c549e07532a1af37986455795112cb9d
Braun Goodson <brn.gdsn@gmail.com>
2024-01-25 08:03:52 -0500

    Update LeafletMap.vue

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="map" style="height: 400px;"></div>
+  <div id="map" :style="{ height: mapHeight + 'px' }"></div>
 </template>
 
 <script setup lang="ts">
-  import { onMounted, onUnmounted, ref } from 'vue';
+  import { onMounted, onUnmounted, ref, watchEffect } from 'vue';
   import L from 'leaflet';
   import 'leaflet/dist/leaflet.css';
 
+  const mapHeight = ref(window.innerHeight);
   const map = ref(null);
+
+  const setMapHeight = () => {
+    mapHeight.value = window.innerHeight;
+  };
   
   onMounted(() => {
+    setMapHeight();
+    
     map.value = L.map('map').setView([51.505, -0.09], 13);
     
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,6 +24,7 @@
     }).addTo(map.value);
 
     window.addEventListener('resize', () => {
+      setMapHeight();
       setTimeout(() => {
         map.value.invalidateSize();
       }, 100);
@@ -28,6 +36,13 @@
     if (map.value) {
       map.value.remove();
     }
+    window.removeEventListener('resize', setMapHeight);
+  });
+
+  watchEffect(() => {
+    if (map.value) {
+      map.value.invalidateSize();
+    }
   });
   
 </script>
@@ -35,6 +50,5 @@
 <style scoped>
   #map { 
     width: 100%; 
-    height: 100%;
   }
 </style>
