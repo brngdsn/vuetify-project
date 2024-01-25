5c1a8b4cbddbc04f4295b1e888faf9ac55504b56
EC2 Default User <ec2-user@ip-172-31-46-80.ec2.internal>
2024-01-25 12:50:24 +0000

    added deps

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    LeafletMap: typeof import('./src/components/LeafletMap.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
