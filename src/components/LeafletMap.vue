363bfdf3a2174511db6eafa6c5e26f03717d93bd
Braun Goodson <brn.gdsn@gmail.com>
2024-01-25 07:40:06 -0500

    Update LeafletMap.vue

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -2,23 +2,18 @@
   <div id="map" style="height: 400px;"></div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-
-export default defineComponent({
-  name: 'LeafletMap',
-  setup() {
-    onMounted(() => {
-      const map = L.map('map').setView([51.505, -0.09], 13);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      }).addTo(map);
-    });
-  }
-});
+<script setup lang="ts">
+  import { onMounted } from 'vue';
+  import L from 'leaflet';
+  import 'leaflet/dist/leaflet.css';
+  
+  onMounted(() => {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+  });
 </script>
 
 <style>
